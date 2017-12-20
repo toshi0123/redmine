2c872c042e38d99e5d730f4360c1364049cef6d6
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-20 16:41:58 +0900

    Update download.sh

--- assets/build/download.sh
+++ assets/build/download.sh
@@ -14,16 +14,22 @@ while read line;do
  sudo -u redmine -H git clone --depth 1 -v $line
 done<<EOF
 https://github.com/tsi/redmine-theme-flat.git
-https://github.com/akabekobeko/redmine-theme-minimalflat2.git
 https://github.com/hardpixel/minelab.git
 https://github.com/makotokw/redmine-theme-gitmike.git
 EOF
+
+cd redmine-theme-flat
+sed -i '/^@import url(.https:/s|^|//|g' sass/application.scss stylesheets/application.css
+cd -
+
+wget https://github.com/akabekobeko/redmine-theme-minimalflat2/releases/download/v1.3.5/minimalflat2-1.3.5.zip -O minimalflat.zip
+unzip minimalflat.zip
+rm -f minimalflat.zip
 
 cd /home/redmine/plugins
 while read line;do
  sudo -u redmine -H git clone --depth 1 -v $line
 done<<EOF
-https://github.com/onozaty/redmine-view-customize.git
 https://github.com/sciyoshi/redmine-slack.git
 https://github.com/hokkey/redmine_chatwork.git
 https://github.com/hidakatsuya/redmine_default_custom_query.git
@@ -32,3 +38,11 @@ https://github.com/alexbevi/redmine_knowledgebase.git
 https://github.com/paginagmbh/redmine_lightbox2.git
 https://github.com/Restream/redmine_tagging.git
 EOF
+
+wget https://bitbucket.org/haru_iida/redmine_theme_changer/downloads/redmine_theme_changer-0.2.0.zip -O redmine_theme_changer-0.2.0.zip
+unzip redmine_theme_changer-0.2.0.zip
+rm -f redmine_theme_changer-0.2.0.zip
+
+wget http://launchpad.net/redminelocalavatars/trunk/0.1.1/+download/redmine_local_avatars-0.1.1.tar.gz -O redmine_local_avatars-0.1.1.tar.gz
+tar zxf redmine_local_avatars-0.1.1.tar.gz
+rm -f redmine_local_avatars-0.1.1.tar.gz
