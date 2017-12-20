19b34586347cf4e0279e2cd13b038b6a002a8243
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-20 16:04:05 +0900

    Update download.sh

--- assets/build/download.sh
+++ assets/build/download.sh
@@ -8,3 +8,28 @@ TAG=`git ls-remote -t https://github.com/redmine/redmine.git | grep -v -e '\^{}'
 echo "Downloading redmine ${TAG}"
 
 sudo -u redmine -H git clone --depth 1 -b ${TAG} https://github.com/redmine/redmine.git redmine -v
+
+cd /home/redmine/public/themes
+while read line;do
+ sudo -u redmine -H git clone --depth 1 -v $line
+done<<EOF
+https://github.com/tsi/redmine-theme-flat.git
+https://github.com/akabekobeko/redmine-theme-minimalflat2.git
+https://github.com/hardpixel/minelab.git
+https://github.com/makotokw/redmine-theme-gitmike.git
+EOF
+
+cd /home/redmine/plugins
+while read line;do
+ sudo -u redmine -H git clone --depth 1 -v $line
+done<<EOF
+https://github.com/onozaty/redmine-view-customize.git
+https://github.com/sciyoshi/redmine-slack.git
+https://github.com/hokkey/redmine_chatwork.git
+https://github.com/hidakatsuya/redmine_default_custom_query.git
+https://github.com/akiko-pusu/redmine_issue_templates.git
+https://github.com/alexbevi/redmine_knowledgebase.git
+https://github.com/paginagmbh/redmine_lightbox2.git
+https://github.com/Restream/redmine_tagging.git
+https://bitbucket.org/haru_iida/redmine_theme_changer.git
+EOF
